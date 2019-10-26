cdf4dff5e43d8be9d25eb6b9a6a72cc49c2a8927
ss <ss0430.b91@gmail.com>
2019-10-27 00:07:04 +0900

    登録処理の修正

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'user_id', 'email', 'password', 'score', 'ip_address', 'created_at', 'updated_at', 'delete_flg'
     ];
 
     /**
